6ec623f1c3978af617140f0e85569c41e0612e74
milreena <123353492+milreena@users.noreply.github.com>
2023-08-18 14:52:49 +0530

    Create arithmaticoperation.py

---
+++ arithmaticoperation.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return (x+y)
+def sub(x,y):
+    return (x-y)
+def mul(x,y):
+    return (x*y)
+def square(x,y):
+    return(x**y)
+def div(x,y):
+    return (x/y)
+def modulus(x,y):
+    return(x%y)
+num1=int(input("enter value of num1"))
+num2=int(input("enter value of num2"))
+print(add(num1,num2))
+print(sub(num1,num2))
+print(mul(num1,num2))
+print(square(num1,num2))
+print(div(num1,num2))
+print(modulus(num1,num2))
