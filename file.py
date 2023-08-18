1bb76070b6575190b25e84726acb4f84041363d7
milreena <123353492+milreena@users.noreply.github.com>
2023-08-18 14:26:00 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,14 @@
+import re
+phone_regex = re.compile(r'\+\d{12}')
+email_regex = re.compile(r'[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}')
+with open('mili.txt', 'r') as f:
+    for line in f:
+
+        matches = phone_regex.findall(line)
+
+        for match in matches:
+            print(match)
+        matches = email_regex.findall(line)
+
+        for match in matches:
+            print(match)
